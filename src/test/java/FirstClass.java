377ffc6d41ee5832b88cd18b40c4fdc0ae419958
KseniyaKudrina <knkudrina@gmail.com>
2023-02-06 19:40:17 +0700

    testOpenNewPage_ClickHowToBuy added and refactor testChangeBGColor_DifferentWeatherButton

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -17,13 +17,14 @@ public class FirstClass extends BaseTest {
     final static By SUPPORT_DROPDOWN = By.id("support-dropdown");
     final static By ASC_A_QUESTION_MENU_DROPDOWN = By.xpath("//li[@class='with-dropdown']//a[contains(@href,'https://home.openweathermap.org/questions')]");
     final static By DIFFERENT_WEATHER_BUTTON = By.xpath("//div[@id='weather-widget']//span[@class='control-el owm-switch']");
+    final static By PRICING_BUTTON = By.linkText("Pricing");
     private void openBaseURL(){
         getDriver().get(BASE_URL);
     }
     private void waitForGrayFrameDissapeared(){
-        getWait30().until(ExpectedConditions.
-                visibilityOfElementLocated(
-                        By.xpath("//div[@class = 'horizontal-section my-5']//span[contains(text(), '© 2012 — 2023 OpenWeather ® All rights reserved')]")));
+        getWait20().until(ExpectedConditions.
+                invisibilityOfElementLocated(
+                        By.className("own-loader-container")));
     }
     private String getText(By by, WebDriver driver){
         return driver.findElement(by).getText();
@@ -53,11 +54,10 @@ public class FirstClass extends BaseTest {
     public void testSelectAscAQuestion() throws InterruptedException {
 
         openBaseURL();
-        Thread.sleep(8000);
         waitForGrayFrameDissapeared();
-        waitElementToBeVisible(SUPPORT_DROPDOWN, getWait30());
-        click(SUPPORT_DROPDOWN, getWait30());
-        click(ASC_A_QUESTION_MENU_DROPDOWN, getWait30());
+        waitElementToBeVisible(SUPPORT_DROPDOWN, getWait10());
+        click(SUPPORT_DROPDOWN, getWait10());
+        click(ASC_A_QUESTION_MENU_DROPDOWN, getWait10());
 
         String winHandleBefore = getDriver().getWindowHandle();
 
@@ -66,7 +66,8 @@ public class FirstClass extends BaseTest {
             getDriver().switchTo().window(winHandle);
         }
 
-        Select SubjectField = new Select(getDriver().findElement(By.xpath("//div[@class='col-sm-8']/select[@id='question_form_subject']")));
+        Select SubjectField = new Select(getDriver().findElement(By.
+                xpath("//div[@class='col-sm-8']/select[@id='question_form_subject']")));
         SubjectField.selectByIndex(2);
 
         //Закрываем новое окно
@@ -96,6 +97,20 @@ public class FirstClass extends BaseTest {
 
         Assert.assertNotEquals(bgColorAfter,bgColorAfterBefore);
     }
+    @Test
+    public void testOpenNewPage_ClickHowToBuy(){
+        String homePageTitle = "Сurrent weather and forecast";
+        openBaseURL();
+        waitForGrayFrameDissapeared();
+        waitElementToBeVisible(PRICING_BUTTON, getWait10());
+        click(PRICING_BUTTON, getWait10());
+
+        Assert.assertNotEquals(homePageTitle,getDriver().getTitle());
+
+
+
+    }
+
 
 
 }
