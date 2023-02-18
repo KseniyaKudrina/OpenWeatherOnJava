d22156385e683e3b7e4479e64b2df07771d7dc71
KseniyaKudrina <knkudrina@gmail.com>
2023-02-18 11:47:19 +0700

    testCountTopMenuTitles added

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class MainPage extends FooterMenuPage{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPage extends FooterMenuPage {
     @FindBy(className = "own-loader-container")
     private WebElement grayContainer;
     @FindBy(xpath = "//div[@class = 'horizontal-section my-5']//span[contains(text(), '© 2012 — 2023 OpenWeather ® All rights reserved')]")
@@ -24,8 +27,11 @@ public class MainPage extends FooterMenuPage{
     @FindBy(xpath = "//div[@class='pop-up-container']")
     private WebElement differentWeatherPopUpContainer;
 
-    @FindBy(xpath ="//div[@class='dropdown-selector']")
+    @FindBy(xpath = "//div[@class='dropdown-selector']")
     private WebElement differentWeatherDdMoreOptionsDdSelector;
+
+    @FindBy(xpath = "//div[@id='desktop-menu']//li")
+    private WebElement sectionsTopMenu;
 
 
     final By ASC_A_QUESTION_MENU_DROPDOWN = By.xpath("//li[@class='with-dropdown']//a[contains(@href,'https://home.openweathermap.org/questions')]");
@@ -35,16 +41,29 @@ public class MainPage extends FooterMenuPage{
     public MainPage(WebDriver driver) {
         super(driver);
     }
-    public void waitForGrayContainerDissappeared(){
+
+    public void waitForGrayContainerDissappeared() {
         getWait().until(ExpectedConditions.
                 invisibilityOfElementLocated(
                         By.className("own-loader-container")));
     }
-    public String getTextName(){
-       return getText(footerNameCompany);
+
+    public String getTextName() {
+        return getText(footerNameCompany);
     }
    /*public void inputSearchCriteria(String text){
        input(text, __ элемент __); нужен элемент для ввода текста
     } */
 
+    public int getWebElementsTopMenu() {
+        List<WebElement> list = getDriver().
+                findElements(By.xpath("//div[@id='desktop-menu']//ul//li"));
+        List<String> textElements = new ArrayList<>();
+        int count = 0;
+        for (WebElement element : list) {
+                textElements.add(element.getText());
+                count++;
+            }
+        return count;
+    }
 }
--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -5,6 +5,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MainPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainTest extends BaseTest {
 
     @Test
@@ -15,6 +18,14 @@ public class MainTest extends BaseTest {
         MainPage mainPage = new MainPage(getDriver());
 
         Assert.assertEquals(mainPage.getTextName(),nameCopyright);
+    }
+    @Test
+    public void testCountTopMenuTitles(){
+        int countTopMenuTitles = 15;
+        openBaseURL();
+        MainPage mainPage = new MainPage(getDriver());
+        Assert.assertEquals(mainPage.getWebElementsTopMenu(), countTopMenuTitles);
+
     }
 
 }
