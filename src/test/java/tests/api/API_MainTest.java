a4f0a324590ffb424c9fc124301e065e9b4d1fa6
KseniyaKudrina <knkudrina@gmail.com>
2023-03-02 21:26:18 +0700

    test_API_CNTResponse_OpenBaseURL() added

--- src/test/java/tests/api/API_MainTest.java
+++ src/test/java/tests/api/API_MainTest.java
@@ -23,4 +23,24 @@ public class API_MainTest extends BaseTest{
             Assert.assertTrue(requests.get(i).contains("openweathermap.org/"));
         }
     }
+    @Test
+    public void test_API_CNTResponse_OpenBaseURL() {
+        List<String> responses = new CaptureNetworkTraffic()
+                .setUpDevTool(getDriver())
+                .captureHttpResponsesContain("weather");
+
+        openBaseURL();
+
+        Assert.assertNotNull(responses);
+        for (int i = 0; i < responses.size(); i += 4) {
+            Assert.assertEquals(responses.get(i), "200");
+        }
+        for (int i = 1; i < responses.size(); i += 4) {
+            Assert.assertEquals(responses.get(i), "OK");
+        }
+        for (int i = 2; i < responses.size(); i += 4) {
+            Assert.assertTrue(responses.get(i).contains("openweathermap.org/"));
+        }
+        Assert.assertTrue(Double.parseDouble(responses.get(3).substring(10, 14)) <= 3);
+    }
 }
